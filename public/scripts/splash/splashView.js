0eec92d7aba7bd93f0cfb5dce9e73735d7982454
Poll Castillo <alejandrocordova2@icloud.com>
2023-11-02 13:28:34 -0500

    update: Splash View

--- public/scripts/splash/splashView.js
+++ public/scripts/splash/splashView.js
@@ -4,13 +4,18 @@ class View {
         const root = document.getElementById('root');
         root.innerHTML = `
       <div class='view'>
-        <div class='view__content splash:content'>
+        <div class='view__content splash__content'>
           <div class='content'>
             <h1 class='uppercase'>Nooise</h1>
             <p class='uppercase'>Personaliza tus sonidos ambientales</p>
 
             <button class='btn btn-get__started' id='start'>Empezar</button>
           </div>
+        </div>
+
+        <div class='background'>
+          <div class='background-layer:blur'></div>
+          <div class='background-layer:circle spin'></div>
         </div>
       </div>
     `;
