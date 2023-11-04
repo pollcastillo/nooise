b93ebc699dc496ea7885806c810841b07769b92a
Poll Castillo <alejandrocordova2@icloud.com>
2023-11-04 00:42:17 -0500

    update(delegates): add more delegate interfaces

--- source/scripts/tools/delegates.ts
+++ source/scripts/tools/delegates.ts
@@ -1,3 +1,13 @@
 export interface ViewDelegate {
     render(): void;
 }
+
+export interface ControllerDelegate {
+  control(): void;
+}
+
+export interface SliderControllerDelegate {
+  next(): void;
+  prev(): void;
+  skip(): void;
+}
